fa1ebb56ebf8726bcf2966fc71c42f84d9d8ff01
SodaCris <18463922396@163.com>
2021-12-22 08:49:53 +0800

    add commented code

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     process.start(name);
     process.waitForFinished(-1); // will wait forever until finished
 
-    QTextCodec* c = QTextCodec::codecForName("GBK");
+    QTextCodec* c = QTextCodec::codecForName("gbk");
 
     auto std_out = c->toUnicode(process.readAllStandardOutput());
 //    auto std_err = c->toUnicode(process.readAllStandardError());
@@ -46,6 +46,22 @@ int main(int argc, char *argv[])
     if(hasMatch){
         std::cout << match.captured(0).toStdString() << "\n";
     }
+
+//    std::cout << "System Locale: " << boost::locale::util::get_system_locale() << "\n";
+//    std::cout << std_out.toStdString() << "\n";
+
+//    boost::locale::generator gen;
+//     std::locale loc(gen(""));
+//     std::locale::global(loc);
+//     std::cout.imbue(loc);
+
+//     std::cout << "LOCALE NAME: " << std::use_facet<boost::locale::info>(loc).name() << std::endl;
+//     std::cout << "LOCALE LANG: " << std::use_facet<boost::locale::info>(loc).language() << std::endl;
+//     std::cout << "LOCALE COUNTRY: " << std::use_facet<boost::locale::info>(loc).country()
+//               << std::endl;
+//     std::cout << "LOCALE ENCODING: " << std::use_facet<boost::locale::info>(loc).encoding()
+//               << std::endl;
+//     std::cout << "LOCALE UTF8: " << std::use_facet<boost::locale::info>(loc).utf8() << std::endl;
     return 0;
 //    return a.exec();
 }
